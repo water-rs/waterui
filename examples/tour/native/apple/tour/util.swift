2201d11f18583f7ff0978d8f15483b6a2d0d5dbc
Lexo Liu <me@lexo.cool>
2023-12-08 15:43:49 +0800

    MacOS support (minimum) & counter example

--- examples/tour/native/apple/tour/util.swift
+++ examples/tour/native/apple/tour/util.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import SwiftUI
+extension WaterUIBuf{
+    func to_data() -> Data{
+        let pointer=UnsafeBufferPointer(start: self.head, count: Int(self.len))
+        return Data(buffer: pointer)
+    }
+    
+    func to_string() -> String{
+        let data=self.to_data()
+        return String(data: data, encoding:.utf8)!
+    }
+}
+
+
+extension WaterUIViews{
+    func to_views() -> [WaterUIViewObject]{
+        let pointer=UnsafeBufferPointer(start: self.head, count: Int(self.len))
+        return Array(pointer)
+    }
+}
