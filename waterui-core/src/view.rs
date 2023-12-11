5a3a16c955774300768942f6474747be335b9d11
Lexo Liu <me@lexo.cool>
2023-12-11 20:03:47 +0800

    increase `when` and `or` extension for View & move ViewExt

--- waterui-core/src/view.rs
+++ waterui-core/src/view.rs
@@ -1,8 +1,3 @@
-use crate::layout::{Alignment, Frame};
-use crate::{layout::Size, modifier::ViewModifier};
-
-use crate::component::TapGesture;
-use crate::modifier::Modifier;
 use std::{
     any::{type_name, TypeId},
     fmt::Debug,
@@ -138,46 +133,5 @@ impl<V: View + ?Sized + 'static> View for Box<V> {
 impl Debug for dyn View {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str("dyn View")
-    }
-}
-
-pub trait ViewExt: View {
-    fn modifier<T: ViewModifier>(self, modifier: T) -> Modifier<T>;
-    fn on_tap(self, event: impl Fn() + 'static) -> TapGesture;
-    fn width(self, size: impl Into<Size>) -> Modifier<Frame>
-    where
-        Self: Sized;
-    fn height(self, size: impl Into<Size>) -> Modifier<Frame>
-    where
-        Self: Sized;
-
-    fn leading(self) -> Modifier<Frame>;
-
-    fn boxed(self) -> BoxView;
-}
-
-impl<V: View + 'static> ViewExt for V {
-    fn modifier<T: ViewModifier>(self, modifier: T) -> Modifier<T> {
-        Modifier::new(self.boxed(), modifier)
-    }
-
-    fn on_tap(self, event: impl Fn() + 'static) -> TapGesture {
-        TapGesture::new(Box::new(self), Box::new(event))
-    }
-
-    fn width(self, size: impl Into<Size>) -> Modifier<Frame> {
-        Modifier::new(self.boxed(), Frame::default().width(size))
-    }
-
-    fn height(self, size: impl Into<Size>) -> Modifier<Frame> {
-        Modifier::new(self.boxed(), Frame::default().height(size))
-    }
-
-    fn leading(self) -> Modifier<Frame> {
-        Modifier::new(self.boxed(), Frame::default().alignment(Alignment::Leading))
-    }
-
-    fn boxed(self) -> BoxView {
-        Box::new(self)
     }
 }
---
+++ waterui/src/view.rs
@@ -0,0 +1,62 @@
+use crate::{
+    layout::{Alignment, Frame, Size},
+    widget::TapGesture,
+    Modifier,
+};
+use waterui_core::modifier::ViewModifier;
+pub use waterui_core::view::*;
+pub trait ViewExt: View {
+    fn modifier<T: ViewModifier>(self, modifier: T) -> Modifier<T>;
+    fn on_tap(self, event: impl Fn() + 'static) -> TapGesture;
+    fn width(self, size: impl Into<Size>) -> Modifier<Frame>
+    where
+        Self: Sized;
+    fn height(self, size: impl Into<Size>) -> Modifier<Frame>
+    where
+        Self: Sized;
+
+    fn leading(self) -> Modifier<Frame>;
+
+    fn when(self, condition: bool) -> BoxView;
+    fn or<Content: View + 'static>(self, condition: bool, content: Content) -> BoxView;
+
+    fn boxed(self) -> BoxView;
+}
+
+impl<V: View + 'static> ViewExt for V {
+    fn modifier<T: ViewModifier>(self, modifier: T) -> Modifier<T> {
+        Modifier::new(self.boxed(), modifier)
+    }
+
+    fn on_tap(self, event: impl Fn() + 'static) -> TapGesture {
+        TapGesture::new(Box::new(self), Box::new(event))
+    }
+
+    fn width(self, size: impl Into<Size>) -> Modifier<Frame> {
+        Modifier::new(self.boxed(), Frame::default().width(size))
+    }
+
+    fn height(self, size: impl Into<Size>) -> Modifier<Frame> {
+        Modifier::new(self.boxed(), Frame::default().height(size))
+    }
+
+    fn leading(self) -> Modifier<Frame> {
+        Modifier::new(self.boxed(), Frame::default().alignment(Alignment::Leading))
+    }
+
+    fn when(self, condition: bool) -> BoxView {
+        self.or(condition, ())
+    }
+
+    fn or<Content: View + 'static>(self, condition: bool, content: Content) -> BoxView {
+        if condition {
+            self.boxed()
+        } else {
+            content.boxed()
+        }
+    }
+
+    fn boxed(self) -> BoxView {
+        Box::new(self)
+    }
+}
