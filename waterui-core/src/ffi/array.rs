a4e6843f3ec8d2988578bac070a467e97851da43
Lexo Liu <me@lexo.cool>
2023-12-13 19:30:00 +0800

    Binding ffi support & TextField support

--- waterui-core/src/ffi/array.rs
+++ waterui-core/src/ffi/array.rs
@@ -5,16 +5,18 @@ macro_rules! impl_array {
         pub struct $name {
             head: *mut $ffi_item,
             len: usize,
+            capacity: usize,
         }
 
         impl From<Vec<$item>> for $name {
             fn from(value: Vec<$item>) -> Self {
+                use std::mem::ManuallyDrop;
                 let value: Vec<$ffi_item> = value.into_iter().map(<$ffi_item>::from).collect();
-                let len = value.len();
-                let boxed = value.into_boxed_slice();
+                let mut value = ManuallyDrop::new(value);
                 Self {
-                    head: Box::into_raw(boxed) as *mut $ffi_item,
-                    len,
+                    head: value.as_mut_ptr() as *mut $ffi_item,
+                    len: value.len(),
+                    capacity: value.capacity(),
                 }
             }
         }
@@ -26,14 +28,17 @@ macro_rules! impl_array {
 pub struct Buf {
     head: *mut u8,
     len: usize,
+    capacity: usize,
 }
 impl From<Vec<u8>> for Buf {
     fn from(value: Vec<u8>) -> Self {
-        let len = value.len();
-        let boxed = value.into_boxed_slice();
+        use std::mem::ManuallyDrop;
+
+        let mut value = ManuallyDrop::new(value);
         Self {
-            head: Box::into_raw(boxed) as *mut u8,
-            len,
+            head: value.as_mut_ptr(),
+            len: value.len(),
+            capacity: value.capacity(),
         }
     }
 }
@@ -41,5 +46,11 @@ impl From<Vec<u8>> for Buf {
 impl From<String> for Buf {
     fn from(value: String) -> Self {
         value.into_bytes().into()
+    }
+}
+
+impl From<Buf> for Vec<u8> {
+    fn from(val: Buf) -> Self {
+        unsafe { Self::from_raw_parts(val.head, val.len, val.capacity) }
     }
 }
