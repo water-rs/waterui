b62c916d9c2264cdef9531dbb27aeec57533d9e5
Lexo Liu <me@lexo.cool>
2023-12-13 19:32:25 +0800

    TextField binding example (need fix)

--- examples/tour/native/apple/water.h
+++ examples/tour/native/apple/water.h
@@ -12,32 +12,31 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-enum WaterUIAlignment {
+typedef enum WaterUIAlignment {
   WaterUIAlignment_Default,
   WaterUIAlignment_Leading,
   WaterUIAlignment_Center,
   WaterUIAlignment_Trailing,
-};
-typedef uint8_t WaterUIAlignment;
+} WaterUIAlignment;
 
-enum WaterUIStackMode {
+typedef enum WaterUIStackMode {
   WaterUIStackMode_Vertical,
   WaterUIStackMode_Horizonal,
-};
-typedef uint8_t WaterUIStackMode;
+} WaterUIStackMode;
 
 typedef struct WaterUIEventObject {
   uintptr_t inner[2];
 } WaterUIEventObject;
 
+typedef struct WaterUIBuf {
+  uint8_t *head;
+  uintptr_t len;
+  uintptr_t capacity;
+} WaterUIBuf;
+
 typedef struct WaterUIViewObject {
   uintptr_t inner[2];
 } WaterUIViewObject;
-
-typedef struct WaterUIBuf {
-  const uint8_t *head;
-  uintptr_t len;
-} WaterUIBuf;
 
 typedef struct WaterUIText {
   struct WaterUIBuf buf;
@@ -45,7 +44,7 @@ typedef struct WaterUIText {
 } WaterUIText;
 
 typedef struct WaterUIButton {
-  struct WaterUIBuf label;
+  struct WaterUIViewObject label;
   struct WaterUIEventObject action;
 } WaterUIButton;
 
@@ -54,20 +53,38 @@ typedef struct WaterUITapGesture {
   struct WaterUIEventObject event;
 } WaterUITapGesture;
 
+typedef struct WaterUIAction {
+  struct WaterUIBuf label;
+  struct WaterUIEventObject action;
+} WaterUIAction;
+
+typedef struct WaterUIActions {
+  struct WaterUIAction *head;
+  uintptr_t len;
+  uintptr_t capacity;
+} WaterUIActions;
+
+typedef struct WaterUIMenu {
+  struct WaterUIViewObject label;
+  struct WaterUIActions actions;
+} WaterUIMenu;
+
+typedef struct WaterUITextField {
+  struct WaterUIBuf label;
+  const void *value;
+  struct WaterUIBuf prompt;
+} WaterUITextField;
+
 typedef enum WaterUISize_Tag {
   WaterUISize_Default,
-  WaterUISize_Px,
-  WaterUISize_Percent,
+  WaterUISize_Size,
 } WaterUISize_Tag;
 
 typedef struct WaterUISize {
   WaterUISize_Tag tag;
   union {
     struct {
-      uintptr_t px;
-    };
-    struct {
-      double percent;
+      double size;
     };
   };
 } WaterUISize;
@@ -87,7 +104,7 @@ typedef struct WaterUIFrame {
   struct WaterUISize min_height;
   struct WaterUISize max_height;
   struct WaterUIEdge margin;
-  WaterUIAlignment alignment;
+  enum WaterUIAlignment alignment;
 } WaterUIFrame;
 
 typedef struct WaterUIFrameModifier {
@@ -96,12 +113,13 @@ typedef struct WaterUIFrameModifier {
 } WaterUIFrameModifier;
 
 typedef struct WaterUIViews {
-  const struct WaterUIViewObject *head;
+  struct WaterUIViewObject *head;
   uintptr_t len;
+  uintptr_t capacity;
 } WaterUIViews;
 
 typedef struct WaterUIStack {
-  WaterUIStackMode mode;
+  enum WaterUIStackMode mode;
   struct WaterUIViews contents;
 } WaterUIStack;
 
@@ -120,6 +138,24 @@ typedef struct WaterUISubscriberBuilderObject {
  * `EventObject` must be valid
  */
 void waterui_call_event_object(struct WaterUIEventObject object);
+
+/**
+ * # Safety
+ * `Binding` must be valid
+ */
+void waterui_drop_string_binding(const void *binding);
+
+/**
+ * # Safety
+ * `Binding` must be valid, and `Buf` is valid UTF-8 string.
+ */
+void waterui_set_string_binding(const void *binding, struct WaterUIBuf string);
+
+/**
+ * # Safety
+ * `Binding` must be valid.
+ */
+struct WaterUIBuf waterui_get_string_binding(const void *binding);
 
 /**
  * # Safety
@@ -144,6 +180,18 @@ int8_t waterui_view_to_button(struct WaterUIViewObject view, struct WaterUIButto
  * `EventObject` must be valid
  */
 int8_t waterui_view_to_tap_gesture(struct WaterUIViewObject view, struct WaterUITapGesture *value);
+
+/**
+ * # Safety
+ * `EventObject` must be valid
+ */
+int8_t waterui_view_to_menu(struct WaterUIViewObject view, struct WaterUIMenu *value);
+
+/**
+ * # Safety
+ * `EventObject` must be valid
+ */
+int8_t waterui_view_to_text_field(struct WaterUIViewObject view, struct WaterUITextField *value);
 
 /**
  * # Safety
@@ -178,6 +226,5 @@ extern void waterui_window_closeable(uintptr_t id, bool is);
 extern void waterui_close_window(uintptr_t id);
 
 extern struct WaterUIViewObject waterui_main(void);
-
 
 #endif /* water_h */
--- examples/tour/src-water/src/lib.rs
+++ examples/tour/src-water/src/lib.rs
@@ -1,35 +1,48 @@
+use std::ops::Deref;
+
+use chrono::Local;
 use waterui::{
-    self,
-    component::{button, stack::vstack, text},
     ffi::utils::{EventObject, ViewObject},
     view,
-    widget::Text,
-    View, ViewExt,
+    widget::{button, text, vstack, when, Action, Menu, TextField},
+    widget::{DatePicker, Text},
+    Binding, View, ViewExt,
 };
 
 #[no_mangle]
 pub extern "C" fn waterui_main() -> ViewObject {
     let view = Home::default();
-    ViewObject::from(Home::default().boxed())
+    ViewObject::from(view.boxed())
 }
 
 #[derive(Debug, Default)]
 #[view]
 struct Home {
     #[state]
-    num: u64,
+    input: String,
+}
+
+struct State<T> {
+    inner: Option<T>,
 }
 
 #[view]
 impl View for Home {
     fn view(&self) -> impl View {
-        let num = self.num.clone();
         vstack((
-            text("Counter"),
-            Text::display(self.num.get()),
-            button("Increase").action(move || {
-                *num.get_mut() += 1;
-            }),
+            TextField::new("field", self.input.clone()), // id:0
+            text("reactive:"),                           // id:1
+            text(self.input.to_string()),                // id:2
         ))
     }
 }
+
+/*
+let a=1;
+(
+    View1(a), // id:UUID
+    Text()    // id:UUID
+)
+
+
+*/
