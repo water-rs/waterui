a480ad164e6c73869392b5747f89be8b9c60c150
Lexo Liu <me@lexo.cool>
2023-12-13 19:32:25 +0800

    TextField binding example (need fix)

--- examples/tour/native/apple/tour/util.swift
+++ examples/tour/native/apple/tour/util.swift
@@ -17,6 +17,15 @@ extension WaterUIBuf{
         let data=self.to_data()
         return String(data: data, encoding:.utf8)!
     }
+    
+    init(_ string:String){
+        self.init()
+        let pointer=UnsafeMutablePointer<UInt8>.allocate(capacity: string.count)
+        string.data(using: .utf8)!.copyBytes(to: pointer,count:string.count)
+        head = pointer
+        len = UInt(string.count)
+        capacity = UInt(string.count)
+    }
 }
 
 
@@ -24,5 +33,44 @@ extension WaterUIViews{
     func to_views() -> [WaterUIViewObject]{
         let pointer=UnsafeBufferPointer(start: self.head, count: Int(self.len))
         return Array(pointer)
+    }
+}
+
+extension WaterUIActions{
+    func to_array() -> [WaterUIAction]{
+        let pointer=UnsafeBufferPointer(start: self.head, count: Int(self.len))
+        return Array(pointer)
+    }
+}
+
+extension WaterUISize{
+    func toCGFloat() -> CGFloat?{
+        switch self.tag{
+        case WaterUISize_Default:
+            return nil
+        case WaterUISize_Size:
+            return CGFloat(self.size)
+        default:
+            abort()
+        }
+    }
+}
+
+
+extension WaterUIAlignment{
+    func toAlignment() -> Alignment?{
+        switch self{
+        case WaterUIAlignment_Default:
+            return nil
+        case WaterUIAlignment_Leading:
+            return .leading
+        case WaterUIAlignment_Trailing:
+            return .trailing
+        case WaterUIAlignment_Center:
+            return .center
+            
+        default:
+            abort()
+        }
     }
 }
