4a5fbf44ebcb599868ca5238318be1fa20e58bb2
Lexo Liu <me@lexo.cool>
2023-12-21 20:07:34 +0800

    Independent reactive crate

---
+++ waterui-reactive/src/reactive.rs
@@ -0,0 +1,151 @@
+use take_mut::take;
+
+use crate::subscriber::Subscriber;
+use std::{
+    fmt::{Debug, Display},
+    hash::Hash,
+    ops::{Deref, DerefMut},
+    sync::Arc,
+    sync::{RwLock, RwLockReadGuard},
+};
+
+pub struct Reactive<T: 'static> {
+    inner: Arc<ReactiveInner<T>>,
+}
+
+impl<T: PartialEq> PartialEq for Reactive<T> {
+    fn eq(&self, other: &Self) -> bool {
+        let left = self.inner.value.read().unwrap();
+        let right = other.inner.value.read().unwrap();
+        left.deref() == right.deref()
+    }
+}
+
+impl<T: Eq> Eq for Reactive<T> {}
+
+impl<T: Hash> Hash for Reactive<T> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.get().hash(state)
+    }
+}
+
+impl<T: Debug> Debug for Reactive<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.get().fmt(f)
+    }
+}
+
+impl<T: Display> Display for Reactive<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.get().fmt(f)
+    }
+}
+
+impl<T> Clone for Reactive<T> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+        }
+    }
+}
+
+impl<T> Reactive<T> {
+    pub fn new(updater: impl 'static + Send + Sync + Fn() -> T) -> Self {
+        Self {
+            inner: Arc::new(ReactiveInner::new(updater)),
+        }
+    }
+
+    pub fn get(&self) -> ReactiveGuard<T> {
+        self.inner.get()
+    }
+
+    pub fn on_update(&mut self, subscriber: impl Into<Subscriber>) {
+        self.inner.on_update(subscriber)
+    }
+
+    pub fn take(&self) -> T {
+        self.inner.take()
+    }
+
+    pub fn need_update(&self) {
+        self.inner.need_update()
+    }
+
+    /// Constructs a `Reactive<T>` from a raw pointer
+    /// # Safety
+    /// The raw pointer must have been previously returned by a call to Reactive<T>::into_raw
+    pub unsafe fn from_raw(ptr: *const ()) -> Self {
+        unsafe {
+            Self {
+                inner: Arc::from_raw(ptr as *const ReactiveInner<T>),
+            }
+        }
+    }
+
+    /// Consumes the Reactive, returning the wrapped pointer.
+    /// To avoid a memory leak the pointer must be converted back to a Reactive using Reactive::from_raw.
+    pub fn into_raw(self) -> *const () {
+        Arc::into_raw(self.inner) as *const ()
+    }
+}
+
+pub struct ReactiveGuard<'a, T> {
+    guard: RwLockReadGuard<'a, Option<T>>,
+}
+
+impl<'a, T> Deref for ReactiveGuard<'a, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        self.guard.as_ref().unwrap()
+    }
+}
+
+type Updater<T> = Box<dyn Send + Sync + Fn() -> T>;
+struct ReactiveInner<T: 'static> {
+    value: RwLock<Option<T>>,
+    updater: RwLock<Updater<T>>,
+}
+
+impl<T> ReactiveInner<T> {
+    pub fn new(updater: impl 'static + Send + Sync + Fn() -> T) -> Self {
+        Self {
+            value: RwLock::new(None),
+            updater: RwLock::new(Box::new(updater)),
+        }
+    }
+
+    pub fn get(&self) -> ReactiveGuard<T> {
+        let mut guard = self.value.write().unwrap();
+        if guard.is_none() {
+            *guard = Some((self.updater.read().unwrap())());
+        }
+
+        ReactiveGuard {
+            guard: self.value.read().unwrap(),
+        }
+    }
+
+    pub fn take(&self) -> T {
+        self.value
+            .write()
+            .unwrap()
+            .take()
+            .unwrap_or((self.updater.read().unwrap())())
+    }
+
+    pub fn on_update(&self, subscriber: impl Into<Subscriber>) {
+        let subscriber = subscriber.into();
+        take(self.updater.write().unwrap().deref_mut(), |value| {
+            Box::new(move || {
+                let new_value = value();
+                subscriber.call();
+                new_value
+            })
+        })
+    }
+
+    pub fn need_update(&self) {
+        self.value.write().unwrap().take();
+    }
+}
---
+++ waterui-reactive/src/subscriber.rs
@@ -0,0 +1,54 @@
+#[repr(C)]
+#[derive(Debug)]
+pub struct Subscriber {
+    state: *mut (),
+    subscriber: unsafe extern "C" fn(*mut ()),
+}
+
+impl<F> From<F> for Subscriber
+where
+    F: FnMut() + Send + Sync,
+{
+    fn from(value: F) -> Self {
+        Self::new(value)
+    }
+}
+
+unsafe impl Send for Subscriber {}
+unsafe impl Sync for Subscriber {}
+
+impl Drop for Subscriber {
+    fn drop(&mut self) {
+        unsafe { drop(Box::from_raw(self.state)) }
+    }
+}
+
+impl Subscriber {
+    pub fn new<F>(f: F) -> Self
+    where
+        F: FnMut() + Send + Sync,
+    {
+        let boxed: Box<Box<dyn FnMut()>> = Box::new(Box::new(f));
+        let state = Box::into_raw(boxed) as *mut ();
+        extern "C" fn from_fn_impl(state: *mut ()) {
+            let boxed = state as *mut Box<dyn Fn()>;
+            unsafe {
+                let f = &*boxed;
+                (f)()
+            }
+        }
+        unsafe { Self::from_raw(state, from_fn_impl) }
+    }
+
+    pub fn call(&self) {
+        unsafe { (self.subscriber)(self.state) }
+    }
+
+    /// Constructs subscribers from raw state and subscriber.
+    /// # Safety
+    /// The subscriber function is marked as unsafe because it requires a raw pointer.
+    /// You must make sure the state and subscriber function is implemented correctly.
+    pub unsafe fn from_raw(state: *mut (), subscriber: unsafe extern "C" fn(*mut ())) -> Self {
+        Self { state, subscriber }
+    }
+}
