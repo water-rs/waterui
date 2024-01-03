f1930472b2d7bcef5ddd7245bbc84fd23bb140c8
Lexo Liu <me@lexo.cool>
2024-01-03 15:08:47 +0800

    Toggle support

---
+++ waterui/src/component/toggle.rs
@@ -0,0 +1,25 @@
+use waterui_reactive::Binding;
+
+use crate::view::IntoView;
+
+use super::AnyView;
+
+pub struct Toggle {
+    pub(crate) label: AnyView,
+    pub(crate) toggle: Binding<bool>,
+}
+
+impl Toggle {
+    pub fn new(label: impl IntoView, toggle: &Binding<bool>) -> Self {
+        Self {
+            label: label.into_anyview(),
+            toggle: toggle.clone(),
+        }
+    }
+}
+
+raw_view!(Toggle);
+
+pub fn toggle(label: impl IntoView, toggle: &Binding<bool>) -> Toggle {
+    Toggle::new(label, toggle)
+}
