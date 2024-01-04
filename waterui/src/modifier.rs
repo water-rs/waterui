a273e8e6806ddd4c83b697d6afd517bf6058f21b
Lexo Liu <me@lexo.cool>
2024-01-04 23:57:34 +0800

    Support conditional render: show modifier

--- waterui/src/modifier.rs
+++ waterui/src/modifier.rs
@@ -1,19 +1,33 @@
-use crate::{component::AnyView, Environment, View};
-pub struct Modifier<T> {
+use crate::{component::AnyView, Environment, IntoReactive, Reactive, View};
+pub struct Modifier<T: 'static> {
     pub(crate) content: AnyView,
-    pub(crate) modifier: T,
+    pub(crate) modifier: Reactive<T>,
 }
 
 impl<T: ViewModifier> Modifier<T> {
-    pub fn new(content: AnyView, modifier: T) -> Self {
-        Self { content, modifier }
+    pub fn new(content: AnyView, modifier: impl IntoReactive<T>) -> Self {
+        Self {
+            content,
+            modifier: modifier.into_reactive(),
+        }
     }
 }
 
-pub trait ViewModifier: Send + Sync {}
+pub trait ViewModifier: Send + Sync + Clone + 'static {}
 
 impl<T: Send + Sync> View for Modifier<T> {
     fn body(self, _env: Environment) -> impl View {
         panic!("You cannot call `view` for a raw view");
+    }
+}
+
+#[derive(Clone)]
+#[repr(transparent)]
+pub struct Display(bool);
+impl ViewModifier for Display {}
+
+impl Display {
+    pub fn new(condition: bool) -> Self {
+        Self(condition)
     }
 }
