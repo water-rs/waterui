9571d9b6bf6caa593d89cdc1db3ecf76bed7092c
Lexo Liu <me@lexo.cool>
2024-04-10 00:07:31 +0800

    New reactive api

--- waterui-reactive/src/compute.rs
+++ waterui-reactive/src/compute.rs
@@ -1,17 +1,71 @@
-use std::{fmt::Display, marker::PhantomData, ops::Deref, sync::Arc};
+use std::{
+    fmt::{Debug, Display},
+    marker::PhantomData,
+    ops::Deref,
+    sync::Arc,
+};
 
-use crate::{subscriber::SharedSubscriberManager, Binding, Subscriber};
+use crate::{
+    subscriber::{SubscribeGuard, SubscribeManage, SubscriberManager},
+    Binding, Subscriber,
+};
 
-pub trait Compute<T> {
-    fn get(&self) -> T;
-    fn subscribe(&self, _subscriber: Subscriber) -> usize {
-        0
+pub trait Compute<T>: Send + Sync {
+    fn compute(&self) -> T;
+    fn register_subscriber(&self, subscriber: Subscriber) -> usize;
+    fn cancel_subscriber(&self, id: usize);
+}
+
+impl<T: Debug + 'static> Debug for Computed<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Compute::<T>::compute(self).fmt(f)
     }
-    fn unsubscribe(&self, _id: usize) {}
+}
+
+impl<T: Display + 'static> Display for Computed<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Compute::<T>::compute(self).fmt(f)
+    }
+}
+
+pub trait ComputeExt<T> {
+    fn subscribe(&self, subscriber: impl Into<Subscriber>) -> SubscribeGuard<'_, Self, T>
+    where
+        Self: Sized + Compute<T>;
+
+    fn transform<Output>(
+        self,
+        transformer: impl 'static + Send + Sync + Fn(T) -> Output,
+    ) -> impl Compute<Output>
+    where
+        T: Send + Sync + 'static;
 
     fn computed(self) -> Computed<T>
     where
-        Self: Sized + 'static,
+        Self: 'static;
+}
+
+impl<V, T> ComputeExt<T> for V
+where
+    Self: Compute<T> + Sized,
+{
+    fn subscribe(&self, subscriber: impl Into<Subscriber>) -> SubscribeGuard<'_, Self, T> {
+        SubscribeGuard::new(self, self.register_subscriber(subscriber.into()))
+    }
+
+    fn transform<Output>(
+        self,
+        transformer: impl 'static + Send + Sync + Fn(T) -> Output,
+    ) -> impl Compute<Output>
+    where
+        T: Send + Sync + 'static,
+    {
+        ComputeTransformer::new(self, transformer)
+    }
+
+    fn computed(self) -> Computed<T>
+    where
+        Self: 'static,
     {
         Computed::from_compute(self)
     }
@@ -29,38 +83,96 @@ impl<T> Clone for Computed<T> {
     }
 }
 
-impl<T> Compute<T> for Computed<T> {
-    fn get(&self) -> T {
-        Compute::get(self.inner.deref())
+impl<T: 'static> Compute<T> for Computed<T> {
+    fn compute(&self) -> T {
+        Compute::compute(self.inner.deref())
+    }
+
+    fn register_subscriber(&self, subscriber: Subscriber) -> usize {
+        Compute::register_subscriber(self.inner.deref(), subscriber)
+    }
+
+    fn cancel_subscriber(&self, id: usize) {
+        Compute::cancel_subscriber(self.inner.deref(), id)
     }
 }
 
-impl<T: Clone> Compute<T> for T {
-    fn get(&self) -> T {
+impl<T: Send + Sync + Clone + 'static> Compute<T> for T {
+    fn compute(&self) -> T {
         self.clone()
+    }
+
+    fn register_subscriber(&self, _subscriber: Subscriber) -> usize {
+        0
+    }
+
+    fn cancel_subscriber(&self, _id: usize) {}
+}
+
+pub struct GroupedCompute<V, const VLEN: usize, T>
+where
+    V: SubscribeManage<T, VLEN>,
+{
+    value: V,
+    guards: [usize; VLEN],
+    subscribers: Arc<SubscriberManager>,
+    _marker: PhantomData<T>,
+}
+
+impl<V1, V2, T1, T2> GroupedCompute<(V1, V2), 2, (T1, T2)>
+where
+    T1: Send + Sync + 'static,
+    T2: Send + Sync + 'static,
+    V1: Compute<T1>,
+    V2: Compute<T2>,
+{
+    pub fn new(v1: V1, v2: V2) -> Self {
+        let subscribers = Arc::new(SubscriberManager::new());
+        let value = (v1, v2);
+        let guards = value.register_subscriber(|| {
+            let subscribers = subscribers.clone();
+            Subscriber::new(move || subscribers.notify())
+        });
+
+        Self {
+            value,
+            subscribers,
+            guards,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<V, const VLEN: usize, T> Drop for GroupedCompute<V, VLEN, T>
+where
+    V: SubscribeManage<T, VLEN>,
+{
+    fn drop(&mut self) {
+        self.value.cancel_subscriber(self.guards)
+    }
+}
+
+impl<V1, V2, T1, T2> Compute<(T1, T2)> for GroupedCompute<(V1, V2), 2, (T1, T2)>
+where
+    T1: Send + Sync + 'static,
+    T2: Send + Sync + 'static,
+    V1: Compute<T1>,
+    V2: Compute<T2>,
+{
+    fn compute(&self) -> (T1, T2) {
+        (self.value.0.compute(), self.value.1.compute())
+    }
+
+    fn register_subscriber(&self, subscriber: Subscriber) -> usize {
+        self.subscribers.register_subscriber(subscriber)
+    }
+
+    fn cancel_subscriber(&self, id: usize) {
+        self.subscribers.cancel_subscriber(id)
     }
 }
 
 impl<T> Computed<T> {
-    pub fn to<F, Output>(&self, transformer: F) -> impl Compute<Output>
-    where
-        F: Fn(T) -> Output,
-    {
-        ComputedTransformer {
-            value: self.clone(),
-            transformer,
-            _marker: PhantomData,
-        }
-    }
-
-    pub fn transform<Output: From<T>>(&self) -> impl Compute<Output> {
-        self.to(Into::into)
-    }
-
-    pub fn try_transform<Output: TryFrom<T>>(&self) -> impl Compute<Result<Output, Output::Error>> {
-        self.to(TryInto::try_into)
-    }
-
     pub fn from_compute(compute: impl Compute<T> + 'static) -> Self {
         Self {
             inner: Arc::new(compute),
@@ -68,103 +180,82 @@ impl<T> Computed<T> {
     }
 }
 
-impl<T: Display> Computed<T> {
-    pub fn display(&self) -> impl Compute<String> {
-        self.to(|v| v.to_string())
+impl<T: Send + Clone + Sync + 'static> Compute<T> for Binding<T> {
+    fn compute(&self) -> T {
+        Compute::compute(&self)
+    }
+
+    fn register_subscriber(&self, subscriber: Subscriber) -> usize {
+        <&Self as Compute<T>>::register_subscriber(&self, subscriber)
+    }
+
+    fn cancel_subscriber(&self, id: usize) {
+        <&Self as Compute<T>>::cancel_subscriber(&self, id)
     }
 }
 
-struct ComputedTransformer<V, T, F> {
+impl<'a, T: Send + Sync + Clone> Compute<T> for &'a Binding<T> {
+    fn compute(&self) -> T {
+        Binding::get(self)
+    }
+
+    fn register_subscriber(&self, subscriber: Subscriber) -> usize {
+        Binding::register_subscriber(self, subscriber)
+    }
+
+    fn cancel_subscriber(&self, id: usize) {
+        Binding::cancel_subscriber(self, id)
+    }
+}
+
+impl<T: Default + Send + Sync + 'static> Compute<T> for Binding<Option<T>> {
+    fn compute(&self) -> T {
+        Binding::take(self).unwrap_or_default()
+    }
+
+    fn register_subscriber(&self, subscriber: Subscriber) -> usize {
+        Binding::register_subscriber(self, subscriber)
+    }
+
+    fn cancel_subscriber(&self, id: usize) {
+        Binding::cancel_subscriber(self, id)
+    }
+}
+
+struct ComputeTransformer<V, T, F> {
     value: V,
     transformer: F,
     _marker: PhantomData<T>,
 }
 
-impl<V, T, F, Output> Compute<Output> for ComputedTransformer<V, T, F>
-where
-    V: Compute<T>,
-    F: Fn(T) -> Output,
-{
-    fn get(&self) -> Output {
-        let t = Compute::get(&self.value);
-        (self.transformer)(t)
-    }
-}
-
-impl<T> Compute<T> for Binding<T> {
-    fn get(&self) -> T {
-        Binding::get(self)
-    }
-
-    fn subscribe(&self, subscriber: Subscriber) -> usize {
-        Binding::subscribe(self, subscriber)
-    }
-
-    fn unsubscribe(&self, id: usize) {
-        Binding::unsubscribe(self, id)
-    }
-}
-
-impl<T1, T2> Computed<(T1, T2)> {
-    pub fn compute<V1, V2, F, Output>(v1: V1, v2: V2, transformer: F) -> impl Compute<Output>
+impl<V, T, F> ComputeTransformer<V, T, F> {
+    pub fn new<Output>(value: V, transformer: F) -> Self
     where
-        V1: Compute<T1>,
-        V2: Compute<T2>,
-        F: Fn(T1, T2) -> Output,
+        F: Fn(T) -> Output,
+        V: Compute<T>,
     {
-        ComputedBuilder::new(v1, v2, transformer)
-    }
-}
-
-struct ComputedBuilder<V, F, T> {
-    values: V,
-    transformer: F,
-    subscribers: SharedSubscriberManager,
-    _marker: PhantomData<T>,
-}
-
-impl<V1, V2, T1, T2, F, Output> ComputedBuilder<(V1, V2), F, (T1, T2)>
-where
-    V1: Compute<T1>,
-    V2: Compute<T2>,
-    F: Fn(T1, T2) -> Output,
-{
-    pub fn new(v1: V1, v2: V2, transformer: F) -> Self {
-        let subscribers = SharedSubscriberManager::new();
-
-        v1.subscribe({
-            let subscribers = subscribers.clone();
-            Subscriber::new(move || subscribers.notify())
-        });
-
-        v2.subscribe({
-            let subscribers = subscribers.clone();
-            Subscriber::new(move || subscribers.notify())
-        });
         Self {
-            values: (v1, v2),
+            value,
             transformer,
-            subscribers,
             _marker: PhantomData,
         }
     }
 }
 
-impl<V1, V2, T1, T2, F, Output> Compute<Output> for ComputedBuilder<(V1, V2), F, (T1, T2)>
+impl<V, T, F, Output> Compute<Output> for ComputeTransformer<V, T, F>
 where
-    V1: Compute<T1>,
-    V2: Compute<T2>,
-    F: Fn(T1, T2) -> Output,
+    F: 'static + Send + Sync + Fn(T) -> Output,
+    V: Compute<T>,
+    T: Send + Sync + 'static,
 {
-    fn get(&self) -> Output {
-        (self.transformer)(self.values.0.get(), self.values.1.get())
+    fn compute(&self) -> Output {
+        (self.transformer)(self.value.compute())
+    }
+    fn register_subscriber(&self, subscriber: Subscriber) -> usize {
+        V::register_subscriber(&self.value, subscriber)
     }
 
-    fn subscribe(&self, subscriber: Subscriber) -> usize {
-        self.subscribers.subscribe(subscriber)
-    }
-
-    fn unsubscribe(&self, id: usize) {
-        self.subscribers.unsubscribe(id);
+    fn cancel_subscriber(&self, id: usize) {
+        V::cancel_subscriber(&self.value, id)
     }
 }
--- waterui-reactive/src/subscriber.rs
+++ waterui-reactive/src/subscriber.rs
@@ -1,76 +1,93 @@
-use std::{
-    collections::BTreeMap,
-    sync::{Arc, RwLock},
-};
+use std::{collections::BTreeMap, marker::PhantomData, sync::RwLock};
 
-#[repr(C)]
-#[derive(Debug)]
+use crate::Compute;
+
 pub struct Subscriber {
-    state: *mut (),
-    subscriber: unsafe extern "C" fn(*mut ()),
+    inner: Box<dyn Fn() + Send + Sync>,
 }
 
-impl<F> From<F> for Subscriber
-where
-    F: Fn() + Send + Sync,
-{
+impl<F: Fn() + Send + Sync + 'static> From<F> for Subscriber {
     fn from(value: F) -> Self {
         Self::new(value)
     }
 }
 
-unsafe impl Send for Subscriber {}
-unsafe impl Sync for Subscriber {}
-
-impl Drop for Subscriber {
-    fn drop(&mut self) {
-        unsafe { drop(Box::from_raw(self.state)) }
-    }
-}
-
 impl Subscriber {
-    pub fn new<F>(f: F) -> Self
-    where
-        F: Fn() + Send + Sync,
-    {
-        let boxed: Box<Box<dyn Fn()>> = Box::new(Box::new(f));
-        let state = Box::into_raw(boxed) as *mut ();
-        extern "C" fn from_fn_impl(state: *mut ()) {
-            let boxed = state as *mut Box<dyn Fn()>;
-            unsafe {
-                let f = &*boxed;
-                (f)()
-            }
-        }
-        unsafe { Self::from_raw(state, from_fn_impl) }
+    pub fn new<F: Fn() + Send + Sync + 'static>(f: F) -> Self {
+        Self { inner: Box::new(f) }
+    }
+
+    pub unsafe fn from_raw(data: *mut (), f: extern "C" fn(*mut ())) -> Self {
+        let function = ExternFunction::new(data, f);
+        Self::new(move || function.call())
     }
 
     pub fn notify(&self) {
-        unsafe { (self.subscriber)(self.state) }
-    }
-
-    /// Constructs subscribers from raw state and subscriber.
-    /// # Safety
-    /// The subscriber function is marked as unsafe because it requires a raw pointer.
-    /// You must make sure the state and subscriber function is implemented correctly.
-    pub unsafe fn from_raw(state: *mut (), subscriber: unsafe extern "C" fn(*mut ())) -> Self {
-        Self { state, subscriber }
+        (self.inner)()
     }
 }
 
+struct ExternFunction {
+    data: *mut (),
+    f: extern "C" fn(*mut ()),
+}
+
+impl ExternFunction {
+    // Warning: You must promise it satisify `Send` and `Sync`.
+
+    pub unsafe fn new(data: *mut (), f: extern "C" fn(*mut ())) -> Self {
+        Self { data, f }
+    }
+
+    pub fn call(&self) {
+        (self.f)(self.data)
+    }
+}
+
+unsafe impl Send for ExternFunction {}
+unsafe impl Sync for ExternFunction {}
+
 pub struct SubscriberManager {
+    inner: RwLock<SubscriberManagerInner>,
+}
+
+impl Default for SubscriberManager {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl SubscriberManager {
+    pub const fn new() -> Self {
+        Self {
+            inner: RwLock::new(SubscriberManagerInner::new()),
+        }
+    }
+    pub fn register(&self, subscriber: Subscriber) -> usize {
+        self.inner.write().unwrap().register(subscriber)
+    }
+
+    pub fn notify(&self) {
+        self.inner.read().unwrap().notify()
+    }
+
+    pub fn cancel(&self, id: usize) {
+        self.inner.write().unwrap().cancel(id)
+    }
+}
+struct SubscriberManagerInner {
     id: usize,
     map: BTreeMap<usize, Subscriber>,
 }
 
-impl SubscriberManager {
+impl SubscriberManagerInner {
     pub const fn new() -> Self {
         Self {
             id: 0,
             map: BTreeMap::new(),
         }
     }
-    pub fn subscribe(&mut self, subscriber: Subscriber) -> usize {
+    pub fn register(&mut self, subscriber: Subscriber) -> usize {
         let id = self
             .id
             .checked_add(1)
@@ -86,31 +103,61 @@ impl SubscriberManager {
         }
     }
 
-    pub fn unsubscribe(&mut self, id: usize) {
+    pub fn cancel(&mut self, id: usize) {
         self.map.remove(&id);
     }
 }
 
-#[derive(Clone)]
-pub struct SharedSubscriberManager {
-    inner: Arc<RwLock<SubscriberManager>>,
+#[doc(hidden)]
+pub trait SubscribeManage<T, const TLEN: usize> {
+    fn register_subscriber(&self, subscriber: impl Fn() -> Subscriber) -> [usize; TLEN];
+    fn cancel_subscriber(&self, guard: [usize; TLEN]);
 }
 
-impl SharedSubscriberManager {
-    pub fn new() -> Self {
+impl<V1, V2, T1, T2> SubscribeManage<(T1, T2), 2> for (V1, V2)
+where
+    V1: Compute<T1>,
+    V2: Compute<T2>,
+{
+    fn register_subscriber(&self, subscriber: impl Fn() -> Subscriber) -> [usize; 2] {
+        [
+            self.0.register_subscriber(subscriber()),
+            self.1.register_subscriber(subscriber()),
+        ]
+    }
+    fn cancel_subscriber(&self, guard: [usize; 2]) {
+        self.0.cancel_subscriber(guard[0]);
+        self.1.cancel_subscriber(guard[1]);
+    }
+}
+
+pub struct SubscribeGuard<'a, V, T>
+where
+    V: Compute<T>,
+{
+    source: &'a V,
+    id: usize,
+    _marker: PhantomData<T>,
+}
+
+impl<'a, V, T> SubscribeGuard<'a, V, T>
+where
+    V: Compute<T>,
+{
+    pub fn new(source: &'a V, id: usize) -> Self {
         Self {
-            inner: Arc::new(RwLock::new(SubscriberManager::new())),
+            source,
+            id,
+            _marker: PhantomData,
         }
     }
-    pub fn subscribe(&self, subscriber: Subscriber) -> usize {
-        self.inner.write().unwrap().subscribe(subscriber)
-    }
+}
 
-    pub fn notify(&self) {
-        self.inner.read().unwrap().notify()
-    }
-
-    pub fn unsubscribe(&self, id: usize) {
-        self.inner.write().unwrap().unsubscribe(id)
+impl<'a, V, T> Drop for SubscribeGuard<'a, V, T>
+where
+    V: Compute<T>,
+{
+    fn drop(&mut self) {
+        self.source.cancel_subscriber(self.id);
     }
 }
