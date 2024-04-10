7365ce3371cf83ac5c8ccb74e1fa2a8c10257bf8
Lexo Liu <me@lexo.cool>
2024-04-11 02:53:04 +0800

    Now every `Compute` will have a single output

--- waterui/src/component/text.rs
+++ waterui/src/component/text.rs
@@ -1,6 +1,6 @@
 use crate::{Compute, Computed};
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct Text {
     pub _content: Computed<String>,
     pub _selectable: Computed<bool>,
@@ -9,19 +9,19 @@ pub struct Text {
 raw_view!(Text);
 
 impl Text {
-    pub fn new(text: impl Compute<String>) -> Self {
+    pub fn new(text: impl Compute<Output = String>) -> Self {
         Self {
             _content: text.computed(),
             _selectable: Computed::constant(true),
         }
     }
 
-    pub fn selectable(mut self, selectable: impl Compute<bool>) -> Self {
+    pub fn selectable(mut self, selectable: impl Compute<Output = bool>) -> Self {
         self._selectable = selectable.computed();
         self
     }
 }
 
-pub fn text(text: impl Compute<String>) -> Text {
+pub fn text(text: impl Compute<Output = String>) -> Text {
     Text::new(text)
 }
