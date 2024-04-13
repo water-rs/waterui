4c64385d1e190ea987a74dba8ccd7ed630dd8d6a
Lexo Liu <me@lexo.cool>
2024-04-14 03:00:50 +0800

    use cbindgen

--- waterui/src/ffi.rs
+++ waterui/src/ffi.rs
@@ -1,27 +1,72 @@
-use std::{any::TypeId, collections::HashMap, mem::ManuallyDrop};
+use std::{mem::ManuallyDrop, ptr::slice_from_raw_parts_mut};
 
-use waterui_reactive::{Binding, Compute, ComputeExt, Computed};
+use waterui_reactive::{Binding, Compute, Computed};
 
-use crate::component::{self, AnyView};
+use crate::{component, Environment, View, ViewExt};
 
 #[repr(C)]
 pub struct Data {
     head: *mut u8,
     len: usize,
-    capacity: usize,
 }
 
-impl From<Vec<u8>> for Data {
-    fn from(mut value: Vec<u8>) -> Self {
-        let head = value.as_mut_ptr();
-        let len = value.len();
-        let capacity = value.capacity();
-
-        Self {
-            head,
-            len,
-            capacity,
+macro_rules! ffi_opaque {
+    ($from:ty,$to:ident,$word:expr) => {
+        #[repr(C)]
+        pub struct $to {
+            inner: [usize; $word],
+            _marker: std::marker::PhantomData<(*const (), std::marker::PhantomPinned)>,
         }
+
+        impl From<$from> for $to {
+            fn from(value: $from) -> Self {
+                unsafe {
+                    Self {
+                        inner: std::mem::transmute(value),
+                        _marker: std::marker::PhantomData,
+                    }
+                }
+            }
+        }
+
+        impl From<$to> for $from {
+            fn from(value: $to) -> Self {
+                unsafe { std::mem::transmute(value.inner) }
+            }
+        }
+    };
+}
+
+#[repr(C)]
+pub struct TypeId {
+    inner: [u64; 2],
+    _marker: std::marker::PhantomData<(*const (), std::marker::PhantomPinned)>,
+}
+
+impl From<std::any::TypeId> for TypeId {
+    fn from(value: std::any::TypeId) -> Self {
+        unsafe {
+            Self {
+                inner: std::mem::transmute(value),
+                _marker: std::marker::PhantomData,
+            }
+        }
+    }
+}
+impl From<TypeId> for std::any::TypeId {
+    fn from(value: TypeId) -> Self {
+        unsafe { std::mem::transmute(value.inner) }
+    }
+}
+
+ffi_opaque!(crate::component::AnyView, AnyView, 2);
+
+impl From<Vec<u8>> for Data {
+    fn from(value: Vec<u8>) -> Self {
+        let len = value.len();
+        let head = Box::into_raw(value.into_boxed_slice()) as *mut u8;
+
+        Self { head, len }
     }
 }
 
@@ -32,13 +77,13 @@ pub struct Utf8Data {
 
 impl From<Data> for Vec<u8> {
     fn from(val: Data) -> Self {
-        unsafe { Vec::from_raw_parts(val.head, val.len, val.capacity) }
+        unsafe { Box::from_raw(slice_from_raw_parts_mut(val.head, val.len)).into_vec() }
     }
 }
 
 impl From<Utf8Data> for String {
     fn from(val: Utf8Data) -> Self {
-        unsafe { String::from_raw_parts(val.inner.head, val.inner.len, val.inner.capacity) }
+        unsafe { String::from_utf8_unchecked(val.inner.into()) }
     }
 }
 
@@ -51,42 +96,31 @@ impl From<String> for Utf8Data {
 }
 
 macro_rules! impl_computed {
-    ($read:ident,$subscribe:ident,$computed_ty:ident,$ty:ty,$output_ty:ty) => {
-        #[repr(C)]
-        pub struct $computed_ty {
-            pointer: *mut $ty,
-        }
-
-        impl From<Computed<$ty>> for $computed_ty {
-            fn from(value: Computed<$ty>) -> Self {
-                Self {
-                    pointer: value.into_raw(),
-                }
-            }
-        }
-
-        impl From<$computed_ty> for Computed<$ty> {
-            fn from(value: $computed_ty) -> Self {
-                unsafe { Self::from_raw(value.pointer) }
-            }
-        }
+    ($read:ident,$subscribe:ident,$unsubscribe:ident,$computed_ty:ident,$ty:ty,$output_ty:ty) => {
+        ffi_opaque!(Computed<$ty>, $computed_ty, 2);
 
         #[no_mangle]
         unsafe extern "C" fn $read(computed: $computed_ty) -> $output_ty {
-            let computed = ManuallyDrop::new(Computed::from_raw(computed.pointer));
+            let computed = ManuallyDrop::new(Computed::from(computed));
             computed.compute().into()
         }
 
         #[no_mangle]
-        unsafe extern "C" fn $subscribe(computed: $computed_ty, subscriber: Subscriber) {
-            let computed = ManuallyDrop::new(Computed::from_raw(computed.pointer));
-            computed.subscribe(move || subscriber.call());
+        unsafe extern "C" fn $subscribe(computed: $computed_ty, subscriber: Subscriber) -> usize {
+            let computed = ManuallyDrop::new(Computed::from(computed));
+            computed.register_subscriber(Box::new(move || subscriber.call()))
+        }
+
+        #[no_mangle]
+        unsafe extern "C" fn $unsubscribe(computed: $computed_ty, id: usize) {
+            let computed = ManuallyDrop::new(Computed::from(computed));
+            computed.cancel_subscriber(id);
         }
     };
 }
 
 macro_rules! impl_binding {
-    ($read:ident,$write:ident,$subscribe:ident,$binding_ty:ident,$ty:ty,$output_ty:ty) => {
+    ($read:ident,$write:ident,$subscribe:ident,$unsubscribe:ident,$binding_ty:ident,$ty:ty,$output_ty:ty) => {
         #[repr(C)]
         pub struct $binding_ty {
             pointer: *const $ty,
@@ -119,9 +153,15 @@ macro_rules! impl_binding {
         }
 
         #[no_mangle]
-        unsafe extern "C" fn $subscribe(binding: $binding_ty, subscriber: Subscriber) {
+        unsafe extern "C" fn $subscribe(binding: $binding_ty, subscriber: Subscriber) -> usize {
             let binding = ManuallyDrop::new(Binding::from_raw(binding.pointer));
-            binding.subscribe(move || subscriber.call());
+            binding.register_subscriber(Box::new(move || subscriber.call()))
+        }
+
+        #[no_mangle]
+        unsafe extern "C" fn $unsubscribe(binding: $binding_ty, id: usize) {
+            let binding = ManuallyDrop::new(Binding::from_raw(binding.pointer));
+            binding.cancel_subscriber(id);
         }
     };
 }
@@ -129,6 +169,7 @@ macro_rules! impl_binding {
 impl_computed!(
     waterui_read_computed_data,
     waterui_subscribe_computed_data,
+    waterui_unsubscribe_computed_data,
     ComputedData,
     Vec<u8>,
     Data
@@ -137,6 +178,7 @@ impl_computed!(
 impl_computed!(
     waterui_read_computed_str,
     waterui_subscribe_computed_str,
+    waterui_unsubscribe_computed_str,
     ComputedUtf8Data,
     String,
     Utf8Data
@@ -146,6 +188,7 @@ impl_binding!(
     waterui_read_binding_str,
     waterui_write_binding_str,
     waterui_subscribe_binding_str,
+    waterui_unsubscribe_binding_str,
     BindingUtf8Data,
     String,
     Utf8Data
@@ -157,6 +200,7 @@ impl_binding!(
     waterui_read_binding_int,
     waterui_write_binding_int,
     waterui_subscribe_binding_int,
+    waterui_unsubscribe_binding_int,
     BindingInt,
     Int,
     Int
@@ -165,6 +209,7 @@ impl_binding!(
 impl_computed!(
     waterui_read_computed_int,
     waterui_subscribe_computed_int,
+    waterui_unsubscribe_computed_int,
     ComputedInt,
     Int,
     Int
@@ -192,49 +237,16 @@ impl Drop for Subscriber {
     }
 }
 
-#[repr(C)]
-struct ViewHandler {
-    data: *const (),
-    f: unsafe extern "C" fn(*const (), *mut AnyView),
-}
-
-impl ViewHandler {
-    pub fn call(&self, view: Box<AnyView>) {
-        unsafe { (self.f)(self.data, Box::into_raw(view)) }
-    }
-}
-
-struct ViewMap {
-    map: HashMap<TypeId, ViewHandler>,
-}
 #[no_mangle]
-unsafe extern "C" fn waterui_insert_view_map(
-    map: *mut ViewMap,
-    view: *mut AnyView,
-    f: ViewHandler,
-) {
-    let mut map = ManuallyDrop::new(Box::from_raw(map));
-    let view = Box::from_raw(view);
-    map.map.insert(view.inner_type_id(), f);
+unsafe extern "C" fn waterui_view_id(view: AnyView) -> TypeId {
+    let view = ManuallyDrop::new(component::AnyView::from(view));
+    view.inner_type_id().into()
 }
 
 #[no_mangle]
-unsafe extern "C" fn waterui_call_view_map(map: *const ViewMap, view: *mut AnyView) {
-    let map = ManuallyDrop::new(Box::from_raw(map as *mut ViewMap));
-    let view = Box::from_raw(view);
-
-    let handler = map.map.get(&view.inner_type_id()).unwrap();
-    handler.call(view);
-}
-#[no_mangle]
-unsafe extern "C" fn waterui_free_view_map(map: *mut ViewMap) {
-    let _ = Box::from_raw(map);
-}
-
-#[no_mangle]
-unsafe extern "C" fn waterui_view_text(view: *mut ()) -> *mut TypeId {
-    let view = ManuallyDrop::new(Box::from_raw(view as *mut AnyView));
-    Box::into_raw(Box::new(view.inner_type_id()))
+unsafe extern "C" fn waterui_call_view(view: AnyView) -> AnyView {
+    let view = component::AnyView::from(view);
+    view.body(Environment::builder().build()).anyview().into()
 }
 
 #[repr(C)]
@@ -256,13 +268,35 @@ impl From<Text> for component::Text {
     }
 }
 
-#[no_mangle]
-unsafe extern "C" fn waterui_view_force_as_text(view: *mut (), text: *mut Text) {
-    let view = *Box::from_raw(view as *mut AnyView);
-    *text = (*view.downcast_unchecked::<component::Text>()).into();
+macro_rules! impl_view {
+    ($ty:ident,$force_as:ident,$id:ident,$free:ident) => {
+        #[no_mangle]
+        unsafe extern "C" fn $force_as(view: AnyView) -> $ty {
+            let view: component::AnyView = view.into();
+            (*view.downcast_unchecked::<component::$ty>()).into()
+        }
+
+        #[no_mangle]
+        unsafe extern "C" fn $free(text: $ty) {
+            let _ = component::$ty::from(text);
+        }
+
+        #[no_mangle]
+        unsafe extern "C" fn $id() -> TypeId {
+            std::any::TypeId::of::<component::$ty>().into()
+        }
+    };
 }
 
-#[no_mangle]
-unsafe extern "C" fn waterui_view_free_text(text: Text) {
-    let _ = component::Text::from(text);
-}
+impl_view!(
+    Text,
+    waterui_view_force_as_text,
+    waterui_view_text_id,
+    waterui_view_free_text
+);
+impl_view!(
+    AnyView,
+    waterui_view_force_as_anyview,
+    waterui_view_anyview_id,
+    waterui_view_free_anyview
+);
