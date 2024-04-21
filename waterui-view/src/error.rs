9f8dd412be93ad6d05ce99b00670e9f86a9ebe8b
Lexo Liu <me@lexo.cool>
2024-04-22 00:35:47 +0800

    Add padding modifier

--- waterui-view/src/error.rs
+++ waterui-view/src/error.rs
@@ -1,42 +1,115 @@
-use crate::{AnyView, View};
+use crate::{AnyView, Environment, View};
 use alloc::boxed::Box;
-use core::fmt::{Debug, Display};
+use core::{
+    any::TypeId,
+    fmt::{Debug, Display},
+    ops::Deref,
+};
 
-pub use anyhow::Error;
+use std::error::Error as StdError;
+pub struct Error {
+    inner: Box<dyn ErrorImpl>,
+}
+
+pub type BoxedStdError = Box<dyn StdError>;
+
+trait ErrorImpl: Debug + Display + 'static {
+    fn body(self: Box<Self>, _env: Environment) -> AnyView;
+    fn type_id(&self) -> TypeId {
+        TypeId::of::<Self>()
+    }
+}
+
+impl<E: StdError + 'static> ErrorImpl for E {
+    fn body(self: Box<Self>, env: Environment) -> AnyView {
+        AnyView::new(env.get::<DefaultErrorView>().unwrap().spawn(self))
+    }
+}
+
+impl Error {
+    pub fn new(error: impl StdError + 'static) -> Self {
+        Self {
+            inner: Box::new(error),
+        }
+    }
+
+    pub fn downcast<T: 'static>(self) -> Result<Box<T>, Self> {
+        if ErrorImpl::type_id(self.inner.deref()) == TypeId::of::<T>() {
+            unsafe { Ok(Box::from_raw(Box::into_raw(self.inner) as *mut T)) }
+        } else {
+            Err(self)
+        }
+    }
+
+    pub fn from_view(view: impl View + 'static) -> Self {
+        Self {
+            inner: Box::new(ErrorView::new(view)),
+        }
+    }
+}
+
+pub struct ErrorView {
+    view: AnyView,
+}
+
+impl ErrorView {
+    fn new(view: impl View + 'static) -> Self {
+        Self {
+            view: AnyView::new(view),
+        }
+    }
+}
+
+impl Display for ErrorView {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.write_fmt(format_args!("ErrorView<{}>", self.view.name()))
+    }
+}
+
+impl Debug for ErrorView {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        Debug::fmt(&self, f)
+    }
+}
+impl ErrorImpl for ErrorView {
+    fn body(self: Box<Self>, _env: Environment) -> AnyView {
+        self.view
+    }
+}
 
 impl View for Error {
     fn body(self, env: crate::Environment) -> impl View {
-        env.get::<DefaultErrorView>().unwrap().spawn(self)
+        self.inner.body(env)
     }
 }
 
 pub trait ResultExt<T, E> {
-    fn error_view<V: View>(self, view: impl FnOnce(E) -> V) -> Result<T, V>
+    fn error_view<V: View + 'static>(self, view: impl FnOnce(E) -> V) -> Result<T, Error>
     where
         Self: Sized;
 }
 
 impl<T, E: Debug + Display + 'static> ResultExt<T, E> for Result<T, E> {
-    fn error_view<V: View>(self, view: impl FnOnce(E) -> V) -> Result<T, V>
+    fn error_view<V: View + 'static>(self, view: impl FnOnce(E) -> V) -> Result<T, Error>
     where
         Self: Sized,
     {
-        self.map_err(view)
+        self.map_err(|error| Error::from_view(view(error)))
     }
 }
 
 pub struct DefaultErrorView {
-    pub builder: Box<dyn Fn(Error) -> AnyView>,
+    pub builder: Box<dyn Fn(BoxedStdError) -> AnyView>,
 }
 
 impl DefaultErrorView {
-    pub fn new<V: View + 'static>(builder: impl 'static + Fn(Error) -> V) -> Self {
+    pub fn new<V: View + 'static>(builder: impl 'static + Fn(BoxedStdError) -> V) -> Self {
         Self {
             builder: Box::new(move |error| AnyView::new(builder(error))),
         }
     }
 
-    pub fn spawn(&self, error: Error) -> AnyView {
+    pub fn spawn(&self, error: BoxedStdError) -> AnyView {
         (self.builder)(error)
     }
 }
--- waterui/src/modifier/mod.rs
+++ waterui/src/modifier/mod.rs
@@ -1,9 +1,30 @@
+mod with;
+pub use with::WithValue;
+
 use core::future::Future;
 
 use waterui_view::{Environment, View};
 
+use crate::layout::Edge;
 pub trait Modifer {
-    fn modify(self, env: Environment, view: impl View) -> impl View;
+    fn modify(self, env: Environment, view: impl View + 'static) -> impl View + 'static;
+}
+
+#[repr(C)]
+pub struct Padding {
+    pub _inner: Edge,
+}
+
+impl Modifer for Padding {
+    fn modify(self, _env: Environment, view: impl View + 'static) -> impl View + 'static {
+        WithValue::new(view, self)
+    }
+}
+
+impl Padding {
+    pub fn new(padding: Edge) -> Self {
+        Self { _inner: padding }
+    }
 }
 
 pub trait ModiferExt: Modifer + Sized {
@@ -28,7 +49,7 @@ impl<M1, M2> And<M1, M2> {
 }
 
 impl<M1: Modifer, M2: Modifer> Modifer for And<M1, M2> {
-    fn modify(self, env: Environment, view: impl View) -> impl View {
+    fn modify(self, env: Environment, view: impl View + 'static) -> impl View + 'static {
         self.m2.modify(env.clone(), self.m1.modify(env, view))
     }
 }
