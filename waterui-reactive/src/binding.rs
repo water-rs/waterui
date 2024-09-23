0a4cd8371caff8d11f0bb4228067e97aafa91e34
Lexo Liu <me@lexo.cool>
2024-09-23 19:52:37 +0800

    Ergonomics improvement

--- waterui-reactive/src/binding.rs
+++ waterui-reactive/src/binding.rs
@@ -1,21 +1,54 @@
-use core::{any::type_name, cell::RefCell, fmt::Debug, ops::RangeBounds};
+use core::{
+    any::type_name,
+    cell::RefCell,
+    fmt::Debug,
+    marker::PhantomData,
+    ops::{Deref, RangeBounds},
+};
 
-use alloc::rc::Rc;
+use alloc::{boxed::Box, rc::Rc};
 
 use crate::{
-    watcher::{SharedWatcherManager, Watcher, WatcherGuard, WatcherManager},
+    watcher::{SharedWatcherManager, Watcher, WatcherGuard},
     Compute,
 };
+
+pub trait CustomBinding<T>: Compute<Output = T> {
+    fn set(&self, value: T);
+}
 
 trait BindingImpl<T> {
     fn get(&self) -> T;
     fn set(&self, value: T);
+    fn add_watcher(&self, watcher: Watcher<T>) -> WatcherGuard;
+    fn cloned(&self) -> Binding<T>;
 }
 
-pub struct Binding<T: 'static> {
-    inner: Rc<dyn BindingImpl<T>>,
-    watchers: SharedWatcherManager<T>,
+impl<B: CustomBinding<T>, T> CustomBinding<T> for &B {
+    fn set(&self, value: T) {
+        CustomBinding::set(*self, value);
+    }
 }
+
+impl<B, T> BindingImpl<T> for B
+where
+    B: CustomBinding<T> + 'static,
+{
+    fn get(&self) -> T {
+        self.compute()
+    }
+
+    fn set(&self, value: T) {
+        CustomBinding::set(self, value);
+    }
+    fn add_watcher(&self, watcher: Watcher<T>) -> WatcherGuard {
+        Compute::add_watcher(self, watcher)
+    }
+    fn cloned(&self) -> Binding<T> {
+        Binding(Box::new(self.clone()))
+    }
+}
+pub struct Binding<T>(Box<dyn BindingImpl<T>>);
 
 impl<T> Debug for Binding<T> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
@@ -23,62 +56,79 @@ impl<T> Debug for Binding<T> {
     }
 }
 
-impl<T: Clone> From<T> for Binding<T> {
-    fn from(value: T) -> Self {
-        Self::from_impl(Container::new(value))
+impl<T: 'static + Clone> Binding<T> {
+    pub fn container(value: T) -> Self {
+        Self::custom(Container::new(value))
     }
 }
 
-impl<T: Clone> Binding<T> {
-    pub fn new(value: impl Into<T>) -> Self {
-        Self::from(value.into())
+impl<T: Default + Clone + 'static> Default for Binding<T> {
+    fn default() -> Self {
+        Self::container(T::default())
     }
+}
+
+pub fn binding<T: 'static + Clone>(value: T) -> Binding<T> {
+    Binding::container(value)
 }
 
 impl<T> Binding<T> {
-    fn from_impl(source: impl BindingImpl<T> + 'static) -> Self {
-        Self {
-            inner: Rc::new(source),
-            watchers: Rc::new(WatcherManager::new()),
-        }
+    pub fn custom(custom: impl CustomBinding<T> + 'static) -> Self {
+        Self(Box::new(custom))
     }
 
     pub fn get(&self) -> T {
-        self.inner.get()
+        self.0.get()
     }
 
     pub fn set(&self, value: T) {
-        self.inner.set(value);
-        self.watchers.notify(|| self.get());
+        self.0.set(value);
     }
 
-    pub fn from_fn(getter: impl 'static + Fn() -> T, setter: impl Fn(T) + 'static) -> Self {
-        Self::from_impl(Custom::new(getter, setter))
+    pub fn map<Output, Getter, Setter>(&self, getter: Getter, setter: Setter) -> Binding<Output>
+    where
+        T: 'static,
+        Output: 'static,
+        Getter: 'static + Fn(T) -> Output,
+        Setter: 'static + Fn(&Binding<T>, Output),
+    {
+        Binding::custom(Map {
+            binding: self.clone(),
+            getter: Rc::new(getter),
+            setter: Rc::new(setter),
+            _marker: PhantomData,
+        })
     }
-}
 
-impl<T: PartialOrd> Binding<T> {
-    pub fn range(self, range: impl RangeBounds<T> + 'static) -> Self {
-        let binding = self.clone();
-        Self::from_fn(
-            move || self.get(),
-            move |new| {
-                if range.contains(&new) {
-                    binding.set(new);
+    pub fn filter(&self, filter: impl 'static + Fn(&T) -> bool) -> Self
+    where
+        T: 'static,
+    {
+        self.map(
+            |value| value,
+            move |binding, value| {
+                if filter(&value) {
+                    binding.set(value);
                 }
             },
         )
     }
 }
+
+impl<T: PartialOrd + 'static> Binding<T> {
+    pub fn range(self, range: impl RangeBounds<T> + 'static) -> Self {
+        self.filter(move |value| range.contains(value))
+    }
+}
 impl Binding<i32> {
     pub fn int(i: i32) -> Self {
-        Self::from(i)
+        Self::container(i)
     }
 }
 
 impl Binding<bool> {
     pub fn bool(value: bool) -> Self {
-        Self::from(value)
+        Self::container(value)
     }
 
     pub fn toggle(&self) {
@@ -88,51 +138,40 @@ impl Binding<bool> {
 
 impl<T> Clone for Binding<T> {
     fn clone(&self) -> Self {
+        self.0.cloned()
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Container<T: Clone> {
+    value: RefCell<T>,
+    watchers: SharedWatcherManager<T>,
+}
+
+impl<T: Clone> Container<T> {
+    pub fn new(value: T) -> Self {
         Self {
-            inner: self.inner.clone(),
-            watchers: self.watchers.clone(),
+            value: RefCell::new(value),
+            watchers: SharedWatcherManager::default(),
         }
     }
 }
 
-struct Container<T>(RefCell<T>);
+impl<T: Clone + 'static> Compute for Container<T> {
+    type Output = T;
+    fn compute(&self) -> Self::Output {
+        self.value.borrow().deref().clone()
+    }
 
-impl<T> Container<T> {
-    pub fn new(value: T) -> Self {
-        Self(RefCell::new(value))
+    fn add_watcher(&self, watcher: Watcher<Self::Output>) -> WatcherGuard {
+        WatcherGuard::from_id(&self.watchers, self.watchers.register(watcher))
     }
 }
 
-struct Custom<Getter, Setter> {
-    getter: Getter,
-    setter: Setter,
-}
-
-impl<Getter, Setter> Custom<Getter, Setter> {
-    pub fn new(getter: Getter, setter: Setter) -> Self {
-        Self { getter, setter }
-    }
-}
-
-impl<T: Clone + 'static> BindingImpl<T> for Container<T> {
-    fn get(&self) -> T {
-        self.0.borrow().clone()
-    }
+impl<T: Clone + 'static> CustomBinding<T> for Container<T> {
     fn set(&self, value: T) {
-        self.0.replace(value);
-    }
-}
-
-impl<T, Getter, Setter> BindingImpl<T> for Custom<Getter, Setter>
-where
-    Getter: Fn() -> T,
-    Setter: Fn(T),
-{
-    fn get(&self) -> T {
-        (self.getter)()
-    }
-    fn set(&self, value: T) {
-        (self.setter)(value);
+        self.value.replace(value.clone());
+        self.watchers.notify(move || value.clone());
     }
 }
 
@@ -143,6 +182,54 @@ impl<T> Compute for Binding<T> {
     }
 
     fn add_watcher(&self, watcher: Watcher<Self::Output>) -> WatcherGuard {
-        WatcherGuard::from_id(&self.watchers, self.watchers.register(watcher))
+        self.0.add_watcher(watcher)
+    }
+}
+
+struct Map<Input, Output, Getter, Setter> {
+    binding: Binding<Input>,
+    getter: Rc<Getter>,
+    setter: Rc<Setter>,
+    _marker: PhantomData<Output>,
+}
+
+impl<Input, Output, Getter, Setter> Clone for Map<Input, Output, Getter, Setter> {
+    fn clone(&self) -> Self {
+        Self {
+            binding: self.binding.clone(),
+            getter: self.getter.clone(),
+            setter: self.setter.clone(),
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<Input, Output, Getter, Setter> Compute for Map<Input, Output, Getter, Setter>
+where
+    Output: 'static,
+    Getter: 'static + Fn(Input) -> Output,
+{
+    type Output = Output;
+    fn compute(&self) -> Self::Output {
+        (self.getter)(self.binding.compute())
+    }
+    fn add_watcher(&self, watcher: Watcher<Self::Output>) -> WatcherGuard {
+        let getter = self.getter.clone();
+        self.binding
+            .add_watcher(Watcher::new(move |value, metadata| {
+                watcher.notify_with_metadata(getter(value), metadata)
+            }))
+    }
+}
+
+impl<Input, Output, Getter, Setter> CustomBinding<Output> for Map<Input, Output, Getter, Setter>
+where
+    Output: 'static,
+
+    Getter: 'static + Fn(Input) -> Output,
+    Setter: Fn(&Binding<Input>, Output),
+{
+    fn set(&self, value: Output) {
+        (self.setter)(&self.binding, value)
     }
 }
