559aa6e4ef575210593b21665f4db4a1f671ae97
Lexo Liu <me@lexo.cool>
2025-09-03 20:41:05 +0800

    refactor: Clean up whitespace and formatting in various files

--- cli/src/template.rs
+++ cli/src/template.rs
@@ -8,16 +8,16 @@ const GITIGNORE_TEMPLATE: &str = include_str!("../templates/gitignore");
 
 pub fn create_project_structure(path: &Path, name: &str) -> Result<()> {
     fs::create_dir_all(path.join("src"))?;
-    
+
     // Write lib.rs from template
     fs::write(path.join("src/lib.rs"), LIB_RS_TEMPLATE)?;
-    
+
     // Generate Cargo.toml using serde
     let cargo_toml = CargoToml::new(name.to_string());
     fs::write(path.join("Cargo.toml"), cargo_toml.to_toml_string()?)?;
-    
+
     // Write .gitignore from template
     fs::write(path.join(".gitignore"), GITIGNORE_TEMPLATE)?;
-    
+
     Ok(())
 }
