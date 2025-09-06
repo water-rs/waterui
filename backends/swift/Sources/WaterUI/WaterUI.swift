53adcea9e1112a6b46c7cd346a4707fdce655554
Lexo Liu <me@lexo.cool>
2025-09-06 23:18:40 +0800

    Fix Xcode

--- backends/swift/Sources/WaterUI/WaterUI.swift
+++ backends/swift/Sources/WaterUI/WaterUI.swift
@@ -1,41 +1,39 @@
 import CWaterUI
 import SwiftUI
 
-class WuiStr{
-    var inner:OpaquePointer
-    var isLeak:Bool = false
-    init(_ inner:OpaquePointer){
-        self.inner=inner
+class WuiStr {
+    var inner: OpaquePointer
+    var isLeak: Bool = false
+    init(_ inner: OpaquePointer) {
+        self.inner = inner
     }
-    
-    init(string:String){
-        self.inner = string.utf8.withContiguousStorageIfAvailable{head in
+
+    init(string: String) {
+        self.inner = string.utf8.withContiguousStorageIfAvailable { head in
             return waterui_str_from_bytes(head.baseAddress, UInt32(string.count))
         }!
     }
-    
-    func toString() -> String{
+
+    func toString() -> String {
         // By ptr and len, copying the data
         let start = waterui_str_as_ptr(self.inner)
         let len = Int(waterui_str_len(self.inner))
-        let buf = UnsafeBufferPointer<UInt8>(start: start, count: len) // do not copy
+        let buf = UnsafeBufferPointer<UInt8>(start: start, count: len)  // do not copy
 
-       
-        return String(decoding: buf, as: UTF8.self) // copy here
+        return String(decoding: buf, as: UTF8.self)  // copy here
 
     }
-    
-    func leak(){
-        self.isLeak  = true
+
+    func leak() {
+        self.isLeak = true
     }
-    
-    deinit{
-        if !isLeak{
+
+    deinit {
+        if !isLeak {
             waterui_str_drop(self.inner)
         }
     }
 }
-
 
 extension WuiTypeId: @retroactive Equatable {
     public static func == (lhs: WuiTypeId, rhs: WuiTypeId) -> Bool {
@@ -44,19 +42,19 @@ extension WuiTypeId: @retroactive Equatable {
 }
 
 @MainActor
-protocol Component:View{
-    static var id: WuiTypeId {get}
-    init(anyview: OpaquePointer,env:WaterUI.Environment)
+protocol Component: View {
+    static var id: WuiTypeId { get }
+    init(anyview: OpaquePointer, env: WaterUI.Environment)
 }
 
 @MainActor
-struct App{
-    var env:WaterUI.Environment
-    init(){
+public struct App:View{
+    var env: WaterUI.Environment
+    public init() {
         self.env = WaterUI.Environment(waterui_init())
     }
-    
-    var main:some View{
+
+    public var body: some View {
         WaterUI.AnyView(anyview: waterui_main(), env: env)
     }
 }
