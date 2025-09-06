278f3a92c95b732275988d41bec8db97a43e2632
Lexo Liu <me@lexo.cool>
2025-09-06 23:18:40 +0800

    Fix Xcode

--- demo/apple/demo/ContentView.swift
+++ demo/apple/demo/ContentView.swift
@@ -6,19 +6,9 @@
 //
 
 import SwiftUI
-
+import WaterUI
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        WaterUI.App()
     }
-}
-
-#Preview {
-    ContentView()
 }
