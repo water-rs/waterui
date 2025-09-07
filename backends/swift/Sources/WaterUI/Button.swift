43fc9ee290b949e56a856db264ffa5da1db743ec
Lexo Liu <me@lexo.cool>
2025-09-07 21:56:57 +0800

    refactor: improve code formatting and readability across multiple files; enhance memory management in Rust FFI

--- backends/swift/Sources/WaterUI/Button.swift
+++ backends/swift/Sources/WaterUI/Button.swift
@@ -8,16 +8,16 @@ import CWaterUI
 import SwiftUI
 
 @MainActor
-struct Button: View ,Component{
+struct Button: View, Component {
     static var id = waterui_button_id()
     private var label: AnyView
     private var action: Action
 
     init(button: WuiButton, env: WaterUI.Environment) {
         label = AnyView(anyview: button.label, env: env)
-        action = Action(inner: button.action,env:env)
+        action = Action(inner: button.action, env: env)
     }
-    
+
     init(anyview: OpaquePointer, env: Environment) {
         self.init(button: waterui_force_as_button(anyview), env: env)
     }
@@ -34,24 +34,24 @@ struct Button: View ,Component{
 @MainActor
 class Action {
     private var inner: OpaquePointer
-    private var env:WaterUI.Environment
-    init(inner: OpaquePointer,env:WaterUI.Environment) {
+    private var env: WaterUI.Environment
+    init(inner: OpaquePointer, env: WaterUI.Environment) {
         self.inner = inner
-        self.env=env
+        self.env = env
     }
 
     func call() {
-        waterui_call_action(self.inner,waterui_clone_env(env.inner))
+        waterui_call_action(self.inner, env.inner)
     }
 
     deinit {
-    
-        weak var this=self
-        Task{@MainActor in
-            if let this=this{
+
+        weak var this = self
+        Task { @MainActor in
+            if let this = this {
                 waterui_drop_action(this.inner)
             }
         }
-       
+
     }
 }
