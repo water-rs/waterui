94f5058033a29157ac9b79ba0d729175e98b5e83
Lexo Liu <me@lexo.cool>
2026-02-28 05:01:15 +0800

    refactor(hydrolysis): remove legacy node/render command skeleton

--- backends/hydrolysis/src/lib.rs
+++ backends/hydrolysis/src/lib.rs
@@ -1,10 +1,3 @@
-use waterui_core::Str;
-use waterui_text::font::ResolvedFont;
-
-pub trait Node {
-    fn render(&self) -> impl Iterator<Item = RenderCommand>;
-}
-
-pub enum RenderCommand {
-    Text { font: ResolvedFont, content: Str },
-}
+//! Hydrolysis backend.
+//!
+//! The legacy `Node`/`RenderCommand` prototype has been removed.
