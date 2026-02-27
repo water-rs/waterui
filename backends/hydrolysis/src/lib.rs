c538d82d21c567c284f1a9538fc0a5ffdf115d67
Lexo Liu <me@lexo.cool>
2026-02-28 05:19:59 +0800

    feat(hydrolysis): add platform abstraction and app runner skeleton

--- backends/hydrolysis/src/lib.rs
+++ backends/hydrolysis/src/lib.rs
@@ -3,5 +3,14 @@
 //! The legacy `Node`/`RenderCommand` prototype has been removed.
 
 mod renderer;
+mod platform;
+mod runner;
 
+pub use platform::{
+    InputEvent, KeyCode, KeyState, Modifiers, OffscreenSurface, OffscreenWindow, PlatformWindow,
+    PointerButton, SurfaceError, SurfaceFrame, SurfaceProvider,
+};
+#[cfg(feature = "winit")]
+pub use platform::WinitWindow;
 pub use renderer::{HydroState, HydrolysisRenderer, RenderContext};
+pub use runner::run;
