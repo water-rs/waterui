0f7699f9295fbb5b112f2259da12601ed07e82e7
Lexo Liu <me@lexo.cool>
2026-03-06 23:16:05 +0800

    Improve GTK backend completeness and filter support

--- backends/gtk/src/components/container.rs
+++ backends/gtk/src/components/container.rs
@@ -8,6 +8,7 @@ use waterui_layout::{StretchAxis, container::FixedContainer};
 use crate::component::GtkComponent;
 use crate::components::fixed_container_widget::WuiFixedContainer;
 use crate::renderer::GtkRenderer;
+use crate::util::effective_stretch_axis;
 
 impl GtkComponent for Native<FixedContainer> {
     fn render(self, env: &Environment, renderer: &mut GtkRenderer) -> Widget {
@@ -16,7 +17,7 @@ impl GtkComponent for Native<FixedContainer> {
         let rendered: Vec<(gtk4::Widget, StretchAxis)> = children
             .into_iter()
             .map(|view| {
-                let axis = view.stretch_axis();
+                let axis = effective_stretch_axis(&view);
                 let widget = renderer.render_any(view, env);
                 (widget, axis)
             })
