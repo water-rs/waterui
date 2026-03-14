a68f2453cc397af35c833ae5ab6030b63d5c6721
Lexo Liu <me@lexo.cool>
2026-03-14 22:32:22 +0800

    Tighten ownership in markdown and inspector

--- src/app.rs
+++ src/app.rs
@@ -59,6 +59,7 @@ impl App {
     }
 
     /// Get an iterator over all windows (main window first).
+    #[must_use]
     pub fn windows(&self) -> impl DoubleEndedIterator<Item = &Window> {
         std::iter::once(&self.main_window).chain(self.windows.iter())
     }
