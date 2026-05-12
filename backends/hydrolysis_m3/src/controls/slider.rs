0f4d8a5b8958bcf15ee077c19f877082c87231ed
Lexo Liu <me@lexo.cool>
2026-05-13 01:22:28 +0800

    Align Material slider shape

--- backends/hydrolysis_m3/src/controls/slider.rs
+++ backends/hydrolysis_m3/src/controls/slider.rs
@@ -1,6 +1,7 @@
 use crate::dimensions::{
-    SLIDER_HORIZONTAL_INSET, SLIDER_HORIZONTAL_SPACING, SLIDER_MIN_TRACK_WIDTH,
-    SLIDER_THUMB_RADIUS, SLIDER_TRACK_HEIGHT, SLIDER_VERTICAL_SPACING,
+    SLIDER_HANDLE_HEIGHT, SLIDER_HANDLE_WIDTH, SLIDER_HORIZONTAL_INSET, SLIDER_HORIZONTAL_SPACING,
+    SLIDER_MIN_TRACK_WIDTH, SLIDER_STATE_LAYER_RADIUS, SLIDER_TRACK_HEIGHT,
+    SLIDER_VERTICAL_SPACING,
 };
 use crate::theme::colors::MaterialColorScheme;
 use crate::theme::state_layer;
@@ -13,7 +14,7 @@ pub const fn metrics() -> SliderMetrics {
         SLIDER_VERTICAL_SPACING,
         SLIDER_MIN_TRACK_WIDTH,
         SLIDER_TRACK_HEIGHT,
-        SLIDER_THUMB_RADIUS,
+        SLIDER_HANDLE_HEIGHT / 2.0,
     )
 }
 
@@ -25,19 +26,29 @@ pub fn draw_track(
 ) {
     draw.fill_rounded_rect(
         track_rect,
-        2.0.into(),
-        &Brush::from(colors.surface_container_highest.peniko()),
+        (SLIDER_TRACK_HEIGHT / 2.0).into(),
+        &Brush::from(colors.secondary_container.peniko()),
     );
-    draw.fill_rounded_rect(fill_rect, 2.0.into(), &Brush::from(colors.primary.peniko()));
+    draw.fill_rounded_rect(
+        fill_rect,
+        (SLIDER_TRACK_HEIGHT / 2.0).into(),
+        &Brush::from(colors.primary.peniko()),
+    );
 }
 
 pub fn draw_thumb(
     colors: &MaterialColorScheme,
     draw: &mut dyn DrawContext,
     center: vello::kurbo::Point,
-    radius: f64,
+    _radius: f64,
 ) {
-    draw.fill_circle(center, radius, &Brush::from(colors.primary.peniko()));
+    let bounds =
+        vello::kurbo::Rect::from_center_size(center, (SLIDER_HANDLE_WIDTH, SLIDER_HANDLE_HEIGHT));
+    draw.fill_rounded_rect(
+        bounds,
+        (SLIDER_HANDLE_WIDTH / 2.0).into(),
+        &Brush::from(colors.primary.peniko()),
+    );
 }
 
 pub fn draw_thumb_state_layer(
@@ -47,5 +58,117 @@ pub fn draw_thumb_state_layer(
     _radius: f64,
     state: WidgetInteractionState,
 ) {
-    state_layer::draw_unbounded_circle(draw, center, 20.0, colors.primary.peniko(), state);
+    state_layer::draw_unbounded_circle(
+        draw,
+        center,
+        SLIDER_STATE_LAYER_RADIUS,
+        colors.primary.peniko(),
+        state,
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use vello::kurbo::{Affine, BezPath, Point, Rect, RoundedRectRadii};
+
+    use super::{MaterialColorScheme, draw_thumb, draw_track, metrics};
+    use crate::dimensions::{
+        SLIDER_HANDLE_HEIGHT, SLIDER_HANDLE_WIDTH, SLIDER_STATE_LAYER_RADIUS, SLIDER_TRACK_HEIGHT,
+    };
+    use crate::{Brush, DrawContext};
+
+    #[derive(Default)]
+    struct RecordingDrawContext {
+        rounded_fills: Vec<(Rect, Brush)>,
+        circle_fills: usize,
+    }
+
+    impl DrawContext for RecordingDrawContext {
+        fn fill_rect(&mut self, _rect: Rect, _brush: &Brush) {}
+
+        fn fill_rounded_rect(&mut self, rect: Rect, _radii: RoundedRectRadii, brush: &Brush) {
+            self.rounded_fills.push((rect, brush.clone()));
+        }
+
+        fn stroke_rect(&mut self, _rect: Rect, _brush: &Brush, _width: f64) {}
+
+        fn stroke_rounded_rect(
+            &mut self,
+            _rect: Rect,
+            _radii: RoundedRectRadii,
+            _brush: &Brush,
+            _width: f64,
+        ) {
+        }
+
+        fn stroke_line(&mut self, _from: Point, _to: Point, _brush: &Brush, _width: f64) {}
+
+        fn stroke_circle(&mut self, _center: Point, _radius: f64, _brush: &Brush, _width: f64) {}
+
+        fn fill_circle(&mut self, _center: Point, _radius: f64, _brush: &Brush) {
+            self.circle_fills += 1;
+        }
+
+        fn fill_path(&mut self, _path: &BezPath, _brush: &Brush) {}
+
+        fn stroke_path(&mut self, _path: &BezPath, _brush: &Brush, _width: f64) {}
+
+        fn push_layer(&mut self, _alpha: f32, _clip: Option<&Rect>) {}
+
+        fn pop_layer(&mut self) {}
+
+        fn push_transform(&mut self, _affine: Affine) {}
+
+        fn pop_transform(&mut self) {}
+    }
+
+    #[test]
+    fn slider_metrics_match_material_web_latest_tokens() {
+        let metrics = metrics();
+
+        assert_eq!(metrics.track_height, SLIDER_TRACK_HEIGHT);
+        assert_eq!(metrics.thumb_radius, SLIDER_HANDLE_HEIGHT / 2.0);
+        assert_eq!(SLIDER_TRACK_HEIGHT, 16.0);
+        assert_eq!(SLIDER_HANDLE_WIDTH, 4.0);
+        assert_eq!(SLIDER_HANDLE_HEIGHT, 44.0);
+        assert_eq!(SLIDER_STATE_LAYER_RADIUS, 20.0);
+    }
+
+    #[test]
+    fn slider_thumb_draws_material_vertical_handle() {
+        let mut draw = RecordingDrawContext::default();
+        draw_thumb(
+            &MaterialColorScheme::baseline_light(),
+            &mut draw,
+            Point::new(64.0, 48.0),
+            SLIDER_HANDLE_HEIGHT / 2.0,
+        );
+
+        assert_eq!(draw.circle_fills, 0);
+        assert_eq!(draw.rounded_fills.len(), 1);
+        assert_eq!(draw.rounded_fills[0].0.width(), SLIDER_HANDLE_WIDTH);
+        assert_eq!(draw.rounded_fills[0].0.height(), SLIDER_HANDLE_HEIGHT);
+    }
+
+    #[test]
+    fn slider_track_uses_material_role_colors() {
+        let colors = MaterialColorScheme::baseline_light();
+        let mut draw = RecordingDrawContext::default();
+        draw_track(
+            &colors,
+            &mut draw,
+            Rect::new(0.0, 0.0, 120.0, SLIDER_TRACK_HEIGHT),
+            Rect::new(0.0, 0.0, 72.0, SLIDER_TRACK_HEIGHT),
+        );
+
+        assert_eq!(draw.rounded_fills.len(), 2);
+        assert!(matches!(
+            &draw.rounded_fills[0].1,
+            Brush::Solid(color) if *color == colors.secondary_container.peniko()
+        ));
+        assert!(matches!(
+            &draw.rounded_fills[1].1,
+            Brush::Solid(color) if *color == colors.primary.peniko()
+        ));
+    }
 }
