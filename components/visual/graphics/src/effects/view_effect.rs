2f7c8b1a48256ee6111ef285a0ad577ea1af8be9
Lexo Liu <me@lexo.cool>
2026-08-09 00:22:06 -0400

    Fix graphics Clippy regressions

--- components/visual/graphics/src/effects/view_effect.rs
+++ components/visual/graphics/src/effects/view_effect.rs
@@ -454,6 +454,7 @@ impl ViewEffectErased {
     }
 
     /// Returns a reference to the child view.
+    #[must_use]
     pub const fn content(&self) -> &waterui_core::AnyView {
         &self.content
     }
